47a79cc505b3fd3239e0462aa8ec29226e4d461e
Svetikova Marina <m-golovleva@yandex.ru>
2022-10-05 18:28:39 +0500

    task45

---
+++ task44_2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+int Readnumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void PrintFiboTon(int N)
+{
+    if(N>= 2)
+    {
+        int[] fibo = new int[N];// массив
+        fibo[0] = 0;//первое число массива по условиям 
+        fibo[1] = 1;//второе число массива по условиям 
+        for (int i = 2; i< fibo.Length; i++) //остальную чатсь массива заполним с помощью цикла
+        {
+            fibo[i]=fibo[i-1]+fibo[i-2];
+        }
+        Console.WriteLine(string.Join("", fibo));
+    }
+    else
+    {
+        Console.WriteLine("N должно быть больше 2");    }
+}
+    int number = Readnumber("Введите N");
+    PrintFiboTon(number);
---
+++ task45/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int[] GetArray(int size, int leftRange,int rightRange)
+{
+     int[] arr = new int[size];
+     Random rand = new Random();
+     for (int i=0; i < size; i++)
+     {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+     }
+     return arr;
+}
+
+int [] arr = GetArray (5,0,10);// здесь arr
+int [] copiedArray = new int [arr.Length]; // что бы работать с массивом под него нужно обязательно выделять память с определенным количеством элементов
+for (int i=0; i<arr.Length; i++)
+{
+    copiedArray[i]=arr[i];// поэлементное копирование массива
+}
+
+Console.WriteLine($"Исходный массив = {string.Join(",", arr)}");
+Console.WriteLine($"Скопированный массив = {string.Join(",", copiedArray)}");
